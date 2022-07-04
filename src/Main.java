ee792e896bdb3a00dc4590790e586dd420db73c1
Stanislav <artemiev.stanislav@gmail.com>
2022-07-04 23:45:02 +0700

    homework 1.10

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        Author author1 = new Author("Фрэнсис", "Фицджеральд");
-        Author author2 = new Author("Федор", "Достоевский");
+        Author authorFitzgerald = new Author("Фрэнсис", "Фицджеральд");
+        Author authorDostoevsky = new Author("Федор", "Достоевский");
 
-        Book book1 = new Book("Великий Гэтсби", author1, 1925);
-        Book book2 = new Book("Братья Карамазовы", author2, 1879);
+        Book bookGreatGatsby = new Book("Великий Гэтсби", authorFitzgerald, 1925);
+        Book bookBrothersKaramazov = new Book("Братья Карамазовы", authorDostoevsky, 1879);
 
-        book2.setYearPublication(1880);
+        bookBrothersKaramazov.setYearPublication(1880);
     }
 }
